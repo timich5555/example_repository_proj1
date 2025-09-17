c872bc007d6c4212d012b07613f1fb1addcc90eb
Тимур Гареев <timonipumba400@gmail.com>
2025-09-17 15:11:10 +0500

    added date and song text

--- proj1_Gareev_Timur.py
+++ proj1_Gareev_Timur.py
@@ -1,6 +1,8 @@
 def main():
     print("Gareev Timur")
     print("IVT-IVS-202B")
+    print("17.09.2025")
+    print("..Стыд за то, что делал не впервой и не в последний..")
 
 if __name__ == '__main__':
     main()
