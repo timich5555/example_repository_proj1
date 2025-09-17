995855e0e6d20e0f2c13df9feb5c325197370b58
Timur <77404739+timich5555@users.noreply.github.com>
2025-09-17 15:14:47 +0500

    new line proj1_Gareev_Timur.py
    Added new line of code

--- proj1_Gareev_Timur.py
+++ proj1_Gareev_Timur.py
@@ -3,6 +3,7 @@ def main():
     print("IVT-IVS-202B")
     print("17.09.2025")
     print("..Стыд за то, что делал не впервой и не в последний..")
+    a = "Easter egg that isnt shown"
 
 if __name__ == '__main__':
     main()
